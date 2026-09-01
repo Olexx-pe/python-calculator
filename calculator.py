811dad2bfe28e44628a7634d33c28cbd99a7f2d1
Olexx-pe <alex.tikhonova14@gmail.com>
2026-09-01 20:15:16 +0500

    я ЗАКОНЧИЛА КАЛЬКУЛЯТОР

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
-bbvbvkjernhvbkvhnbgrthjeghreejkgherjkhfynjevrhbyierutkherktuierkhgtjetbrthuerrhtkbury
+a=int(input("введите число 1"))
+b=int(input("введите число 2"))
+print("один это сумма два минус 3 это умножить 4 это деление")
+с=(input())
+if(c==1):
+    print(a+b)
+elif(c==2):
+    print(a-b)
+elif(c==3):
+    print(a*b)
+elif(c==4):
+    if(b==0):
+        print("На ноль делить нельзя")
+    else:
+        print(a/b)
+else:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    print("всего")
